a6e4fa6a40fe0da9dbc7572f8dd2a2b191aff928
James Hunt <hello@jameshunt.com.au>
2025-09-21 21:24:06 +1000

    fix: remove build

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-gcloud run deploy ck-worker \
-  --image us-east4-docker.pkg.dev/YOUR_PROJECT_ID/cloud-build-repo/ck-worker:[IMAGE_TAG_FROM_BUILD_LOGS] \
-  --platform managed \
-  --region us-east4 \
-  --allow-unauthenticated \
-  --set-env-vars="DEEPGRAM_API_KEY=YOUR_DEEPGRAM_API_KEY" \
-  --session-affinity
-  # ... any other flags you need
